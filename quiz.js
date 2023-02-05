da53c711271bc2c480685c74502b8593995bf78e
Atif Hussain <atifhussain28@gmail.com>
2023-02-06 01:40:09 +0530

    added questions

---
+++ quiz.js
@@ -0,0 +1,35 @@
+const data=[
+    {
+        id:1,
+        question:"Where is taj mahal?",
+        answers:[
+            {answer:"Mumbai", isCorrect:false},
+            {answer:"Pune", isCorrect:false},
+            {answer:"Agra", isCorrect:true},
+            {answer:"Hyderabad", isCorrect:false},
+
+        ],
+    },
+    {
+        id:2,
+        question:"Where is Burj Khalifa?",
+        answers:[
+            {answer:"India", isCorrect:false},
+            {answer:"Abu Dhabi", isCorrect:true},
+            {answer:"Saudi Arabia", isCorrect:false},
+            {answer:"Kuwait", isCorrect:false},
+
+        ],
+    },
+    {
+        id:3,
+        question:"Capital of Pakistan?",
+        answers:[
+            {answer:"Islamabad", isCorrect:true},
+            {answer:"Peshawar", isCorrect:false},
+            {answer:"Lahore", isCorrect:false},
+            {answer:"Balochistan", isCorrect:false},
+
+        ],
+    },
+]
